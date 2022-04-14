6bcabd87df231c8584c3742b62504fe120bf39c6
Zachary V Lederman <zlederma@borg07.cs.purdue.edu>
2022-04-14 18:05:21 -0400

    Commit

--- myhttp.cc
+++ myhttp.cc
@@ -8,14 +8,13 @@
 using namespace std;
 
 
-
-static HTTPRequest::HTTPRequest* HTTPMessageFactory::parseMessage(string raw){
-			cout << raw << endl;
-			return NULL;
-}
-
 HTTPRequest::HTTPRequest(requestType request, string asset, vector<string> headers){
 			_request = request;
 			_asset = asset;
 			_headers = headers;	
 };	
+static HTTPRequest* HTTPMessageFactory::parseMessage(string raw){
+			cout << raw << endl;
+			return NULL;
+}
+
