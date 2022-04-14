a76946c16a5f3f5ec25675a0347a24746086b852
Zachary V Lederman <zlederma@borg07.cs.purdue.edu>
2022-04-14 19:15:32 -0400

    Commit

--- myhttp.cc
+++ myhttp.cc
@@ -21,10 +21,10 @@ HTTPRequest* HTTPMessageFactory::parseMessage(string raw){
 
 vector<string> splitRaw(string raw){
 	
-	vector<string tokens;
+	vector<string> tokens;
 	string delimeter = std::string("\015\012");
 	size_t pos = 0;
-	std::string token;
+	string token;
 	while((pos = raw.find(delimeter)) != string::npos){
 		token = raw.substr(0,pos);
 		raw.erase(0,pos + delimeter.length());
