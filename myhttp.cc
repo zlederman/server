f77e1c231c8a12eb83a51b36004955ca60d336d4
Zachary V Lederman <zlederma@data.cs.purdue.edu>
2022-04-21 15:42:10 -0400

    Commit

--- myhttp.cc
+++ myhttp.cc
@@ -53,7 +53,7 @@ int HTTPResponse::loadRaw(int contentLength, char* raw){
 		response += "\r\n";
 	}
 	response += "\r\n";
-	memcpy(raw,response->c_str(),response.length());
+	memcpy(raw,response.c_str(),response.length());
 	memcpy(raw + response.length(), _body, contentLength);
 	return response.length() + contentLength;
 
