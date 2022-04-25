296b12c424577610411d5acd2dc2c73724e14b55
Zachary V Lederman <zlederma@data.cs.purdue.edu>
2022-04-25 16:29:44 -0400

    Commit

--- dirbrowser.cc
+++ dirbrowser.cc
@@ -1,6 +1,25 @@
+#include <string>
+#include <stdio.h>
+#include <vector>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include "myhttp.hh"
 #include "dirbrowser.hh"
 using namespace std;
+
+
+void loadFile(string asset, HTTPResponse* httpRes){
+	/* */
+}
+
 void DirBrowser::serveAsset(string asset, HTTPResponse* httpRes){
-	return;
+	struct stat attr;
+	stat(path,&attr);
+	if(S_ISREG(attr.st_mode)){
+		loadFile(asset,httpRes);
+	}
+	if(S_ISDIR(attr.st_mode)){
+		/*  */
+	}
 }
