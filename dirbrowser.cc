7a8ae4022e081f28c0d413d99de7aeb1373d7ff3
Zachary V Lederman <zlederma@data.cs.purdue.edu>
2022-04-25 16:32:32 -0400

    Commit

--- dirbrowser.cc
+++ dirbrowser.cc
@@ -10,12 +10,18 @@ using namespace std;
 
 
 void loadFile(string asset, HTTPResponse* httpRes){
-	/* */
+	FILE* f;
+	f = fopen(asset.c_str(),"r");
+	fseek(f,0,SEEK_END);
+	httpRes->_bodySize = ftell(f);
+	httpRes->_body = new char[httpRes->_bodySize];
+	rewind(f);
+	fread(httpRes->_body, sizeof(char),httpRes->_bodySize,f);
 }
 
 void DirBrowser::serveAsset(string asset, HTTPResponse* httpRes){
 	struct stat attr;
-	stat(asset.c_str(),&attr);
+	stat(asset.c_str(), &attr);
 	if(S_ISREG(attr.st_mode)){
 		loadFile(asset,httpRes);
 	}
