9f108d450f5a800c7d83c6acf1849f99948fe70e
Zachary V Lederman <zlederma@data.cs.purdue.edu>
2022-04-26 15:48:03 -0400

    Commit

--- dirbrowser.cc
+++ dirbrowser.cc
@@ -189,7 +189,7 @@ void loadDire(string asset,HTTPResponse* httpRes, vector<string> params){
 	entries = sortBy(entries,params);	
 	rawHTML = assembleHTML(entries);
 	httpRes->_bodySize = rawHTML.length(); 
-	httpRes->_body = new char[httpRes->_bodySize];
+	//httpRes->_body = new char[httpRes->_bodySize];
 	//strcpy(httpRes->_body,rawHTML.c_str());
 	closedir(dir);
 }
