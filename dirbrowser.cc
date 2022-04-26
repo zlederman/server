f9921a76c284636d278cd30fffb27526caa8db5c
Zachary V Lederman <zlederma@data.cs.purdue.edu>
2022-04-26 15:52:20 -0400

    Commit

--- dirbrowser.cc
+++ dirbrowser.cc
@@ -23,7 +23,7 @@ string topDirTemplate =
 	"<body>"
 	"<h1>dir-name</h1>"
 	"<table>"
-	"<tr><th valign=\"top\"><img src=\"\" alt=\"[ICO]\"></th><th><a href=\"?C=N;O=D\">Name</a></th><th><a href=\"?C=M;O=A\">Last modified</a></th><th><a href=\"?C=S;O=A\">Size</a></th><th><a href=\"?C=D;O=A\">Description</a></th></tr>";
+	"<tr><th valign=\"top\"><img src=\"/icons/blank.xbm\" alt=\"[ICO]\"></th><th><a href=\"?C=N;O=D\">Name</a></th><th><a href=\"?C=M;O=A\">Last modified</a></th><th><a href=\"?C=S;O=A\">Size</a></th><th><a href=\"?C=D;O=A\">Description</a></th></tr>";
 
 string bottomDirTemplate = 
 	"<tr><th colspan=\"5\"><hr></th></tr>"
@@ -42,7 +42,7 @@ string getIcon(string fname, enum type _type){
 		return string("/icons/menu.gif");
 	}
 	if(_type == dir){
-		return string("/icons/index.gif");
+		return string("/icons/menu.gif");
 	}
 	if((extIdx = fname.find(".")) == string::npos){
 		return string("/icons/unknown.xbm");
