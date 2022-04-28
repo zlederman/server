2ac7ba9d3096cfa4d8758a4ed87aee77a4e88e88
Zachary V Lederman <zlederma@data.cs.purdue.edu>
2022-04-28 15:05:25 -0400

    Commit

--- dirbrowser.cc
+++ dirbrowser.cc
@@ -23,7 +23,7 @@ string topDirTemplate =
 	"<body>"
 	"<h1>dir-name</h1>"
 	"<table>"
-	"<tr><th valign=\"top\"><img src=\"index.gif\" alt=\"[ICO]\"></th><th><a href=\"?C=N;O=D\">Name</a></th><th><a href=\"?C=M;O=A\">Last modified</a></th><th><a href=\"?C=S;O=A\">Size</a></th><th><a href=\"?C=D;O=A\">Description</a></th></tr>";
+	"<tr><th valign=\"top\"><img src=\"/icons/index.gif\" alt=\"[ICO]\"></th><th><a href=\"?C=N;O=D\">Name</a></th><th><a href=\"?C=M;O=A\">Last modified</a></th><th><a href=\"?C=S;O=A\">Size</a></th><th><a href=\"?C=D;O=A\">Description</a></th></tr>";
 	
 string bottomDirTemplate = 
 	"<tr><th colspan=\"5\"><hr></th></tr>"
