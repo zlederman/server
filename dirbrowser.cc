339cbce1d7a7994b5cd9c136a2841233df870116
Zachary V Lederman <zlederma@data.cs.purdue.edu>
2022-04-28 15:03:05 -0400

    Commit

--- dirbrowser.cc
+++ dirbrowser.cc
@@ -23,7 +23,7 @@ string topDirTemplate =
 	"<body>"
 	"<h1>dir-name</h1>"
 	"<table>"
-	"<tr><th valign=\"top\"><img src=\"\" alt=\"[ICO]\"></th><th><a href=\"?C=N;O=D\">Name</a></th><th><a href=\"?C=M;O=A\">Last modified</a></th><th><a href=\"?C=S;O=A\">Size</a></th><th><a href=\"?C=D;O=A\">Description</a></th></tr>";
+	"<tr><th valign=\"top\"><img src=\"blank.xbm\" alt=\"[ICO]\"></th><th><a href=\"?C=N;O=D\">Name</a></th><th><a href=\"?C=M;O=A\">Last modified</a></th><th><a href=\"?C=S;O=A\">Size</a></th><th><a href=\"?C=D;O=A\">Description</a></th></tr>";
 	
 string bottomDirTemplate = 
 	"<tr><th colspan=\"5\"><hr></th></tr>"
@@ -45,7 +45,7 @@ string getIcon(string fname, enum type _type){
 		return string("/icons/menu.gif");
 	}
 	if((extIdx = fname.find(".")) == string::npos){
-		return string("/icons/unknown.xbm");
+		return string("/icons/binary.xbm");
 	}
 	ext = fname.substr(extIdx, fname.length() - extIdx);
 	if(ext == string(".gif") || ext == string(".png") || ext == string(".xbm")){
